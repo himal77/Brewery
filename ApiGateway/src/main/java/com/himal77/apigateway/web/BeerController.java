fd11128cb18ae2ea48d61ec2b5a579c46a8f9a2d
Himal <purihimal9@gmail.com>
2022-06-20 18:55:16 +0200

    Successfully made api gateway get call

--- ApiGateway/src/main/java/com/himal77/apigateway/web/BeerController.java
+++ ApiGateway/src/main/java/com/himal77/apigateway/web/BeerController.java
@@ -4,35 +4,36 @@ import com.himal77.apigateway.config.BaseUrlConfig;
 import com.himal77.apigateway.domain.Beer;
 import com.himal77.apigateway.domain.BeerStyleEnum;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import java.math.BigDecimal;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 @RequestMapping("/beers")
 @RestController
 public class BeerController {
 
     private final BaseUrlConfig baseUrlConfig;
+    private final RestTemplate restTemplate;
 
     @Autowired
     public BeerController(BaseUrlConfig baseUrlConfig) {
-     this.baseUrlConfig = baseUrlConfig;
+        this.baseUrlConfig = baseUrlConfig;
+        this.restTemplate = new RestTemplate();
     }
 
     @GetMapping
-    public ResponseEntity<Object> findAll(@RequestParam(required = false) String beerUpc) {
-        Beer beer = Beer.builder()
-                .upc(baseUrlConfig.getBeerurl())
-                .beerName("Mango Bobs")
-                .beerStyle(BeerStyleEnum.IPA)
-                .price(BigDecimal.valueOf(12.23))
-                .build();
-
-        return new ResponseEntity<Object>(beer, HttpStatus.OK);
+    public ResponseEntity<Object> findAll(@RequestParam(required = false) String beerUpc) throws URISyntaxException {
+        URI uri = new URI(baseUrlConfig.getBeerurl());
+        ResponseEntity<Object> response = restTemplate.getForEntity(uri, Object.class);
+        return new ResponseEntity<Object>(response.getBody(), response.getStatusCode());
     }
 }
